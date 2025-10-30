89d2c23ba924aab260e6ca870d6280863eed1fec
Martin Briand <martinbriands@gmail.com>
2025-10-29 22:34:33 -0400

    chapter 3 initial push

---
+++ Assets/C3PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class C3PlayerMovement : MonoBehaviour
+{
+    public float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = new Vector2();
+
+        //write your code here
+
+        direction.x = 1;
+        direction.y = 0;
+
+        //end of your code here
+
+        MovePlayer(direction);
+    }
+    
+    private void MovePlayer(Vector2 direction)
+    {
+        transform.position += new Vector3(direction.x, direction.y, 0) * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/C3PlayerMovementInput.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C3PlayerMovementInput : MonoBehaviour
+{
+    public float speed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = new Vector2();
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            // 3.6.1
+            //your code here
+            direction.x = 1;
+            //end of your code
+        }
+
+        //3.6.2
+        //your code here
+        if (Input.GetKey(KeyCode.None))
+        {
+
+        }
+        //end of your code here
+
+        //3.6.3
+
+        //end of your code here
+
+        direction.Normalize();
+
+        MovePlayer(direction);
+    }
+    
+    private void MovePlayer(Vector2 direction)
+    {
+        transform.position += new Vector3(direction.x, direction.y, 0) * speed * Time.deltaTime;
+    }
+}
