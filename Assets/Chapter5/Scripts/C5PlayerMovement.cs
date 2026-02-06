d095a1c8d23c507ce5b2b9f23c3bafad3e3a9187
LeTitouLapinou <162844593+LeTitouLapinou@users.noreply.github.com>
2026-02-06 17:09:07 -0500

    Added Chapter5 base

---
+++ Assets/Chapter5/Scripts/C5PlayerMovement.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class C5PlayerMovement : MonoBehaviour
+{
+    public float speed = 1f;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+    InputAction moveAction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        moveAction = InputSystem.actions.FindAction("Move");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // direction est un vector2 egal a l'input de notre joueur
+        // (1, 0) is on va vers la droite
+        // (0, -1) si on va vers le bas etc...
+        Vector2 direction = moveAction.ReadValue<Vector2>();
+
+        // votre code ici
+
+        bool isMoving = false;
+        if (direction.magnitude > 0)
+        {
+            isMoving = true;
+        }
+        animator.SetBool("isMoving", isMoving);
+
+        //fin de votre code
+
+        spriteRenderer.flipX = direction.x > 0;
+
+        MovePlayer(direction);
+    }
+
+    private void MovePlayer(Vector2 direction)
+    {
+        transform.position += new Vector3(direction.x, direction.y, 0) * speed * Time.deltaTime;
+    }
+}
